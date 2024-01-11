fe850a4d522054e1e376934d1afb29450ed099a0
Ramji Dhruw <ramdhruw7482@gmail.com>
2024-01-12 04:33:04 +0530

    initial commit

---
+++ CollegeApp/app/src/main/java/com/example/classlink/ui/home/HomeFragment.java
@@ -0,0 +1,77 @@
+package com.example.classlink.ui.home;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.classlink.R;
+import com.smarteist.autoimageslider.DefaultSliderView;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderLayout;
+
+public class HomeFragment extends Fragment {
+
+    private SliderLayout sliderLayout;
+    private ImageView map;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        sliderLayout = view.findViewById(R.id.slider);
+        sliderLayout.setIndicatorAnimation(IndicatorAnimations.FILL);
+        sliderLayout.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderLayout.setScrollTimeInSec(1);
+
+        setSliderViews();
+
+        map = view.findViewById(R.id.map);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMap();
+            }
+        });
+
+        return view;
+    }
+
+    private void openMap() {
+        Uri uri = Uri.parse("geo:0, 0?q=Galgotias College of Engineering & Technology, Greater Noida");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.setPackage("com.google.android.apps.maps");
+        startActivity(intent);
+    }
+
+    private void setSliderViews() {
+        for (int i = 0; i< 4; i++){
+            DefaultSliderView sliderView = new DefaultSliderView(getContext());
+            switch (i){
+                case 0:
+                    sliderView.setImageDrawable(R.drawable.loc);
+                    break;
+                case 1:
+                    sliderView.setImageDrawable(R.drawable.img3);
+                    break;
+                case 2:
+                    sliderView.setImageDrawable(R.drawable.img2);
+                    break;
+                case 3:
+                    sliderView.setImageDrawable(R.drawable.img4);
+                    break;
+            }
+            sliderView.setImageScaleType(ImageView.ScaleType.CENTER_CROP);
+
+            sliderLayout.addSliderView(sliderView);
+        }
+    }
+}
